b7112bf709a0215fd7c655d3b331e4de9587427c
sunitagajurel <sunita.gajurel49@gmail.com>
2023-05-18 22:32:27 -0700

    burrow and return implemented

---
+++ data.js
@@ -0,0 +1,21 @@
+const result = {
+    "recordsets": [
+        [
+            {
+                "quantity": 0
+            }
+        ]
+    ],
+    "recordset": [
+        {
+            "quantity": 0
+        }
+    ],
+    "output": {},
+    "rowsAffected": [
+        1
+    ]
+}
+
+
+console.log(result.recordset[0].quantity)
